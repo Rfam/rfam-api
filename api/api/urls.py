3a6a7c4d0302841837d2c1af5d374c35e8c8635e
Anton Petrov <apetrov@ebi.ac.uk>
2017-08-07 14:53:37 +0100

    Add DRF endpoint for Genomes

--- api/api/urls.py
+++ api/api/urls.py
@@ -13,9 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+
+
+
+
+from api.models import Genome
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class GenomeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Genome
+        fields = ('upid', 'ncbi_id', 'length', 'description', 'scientific_name', 'common_name')
+
+# ViewSets define the view behavior.
+class GenomeViewSet(viewsets.ModelViewSet):
+    queryset = Genome.objects.all()
+    serializer_class = GenomeSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'genomes', GenomeViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
