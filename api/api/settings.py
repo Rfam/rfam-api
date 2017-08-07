0bedb5b3c8e5b26fc786c9d1ff4781cd6bc648c4
Anton Petrov <apetrov@ebi.ac.uk>
2017-08-07 14:52:15 +0100

    Load `local_settings.py` if available

--- api/api/settings.py
+++ api/api/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *  # pylint: disable=W0401, W0614
+except ImportError:
+    pass
