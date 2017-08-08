4aace13a26f60c48963bda52e2bb193231ed31fe
Anton Petrov <apetrov@ebi.ac.uk>
2017-08-08 16:15:38 +0100

    Added Family, Taxonomy, Rfamseq, FullRegion endpoints

--- api/api/models.py
+++ api/api/models.py
@@ -24,3 +24,56 @@ class Genome(models.Model):
 
     class Meta:
         db_table = 'genome'
+
+
+class Family(models.Model):
+    rfam_acc = models.CharField(max_length=7, primary_key=True)
+    rfam_id = models.CharField(max_length=40, db_index=True)
+    description = models.CharField(max_length=75)
+    type = models.CharField(max_length=50, db_index=True)
+    num_seed = models.IntegerField()
+    num_full = models.IntegerField()
+    number_of_species = models.IntegerField()
+    author = models.TextField()
+
+    class Meta:
+        db_table = 'family'
+
+
+class Rfamseq(models.Model):
+    rfamseq_acc = models.CharField(max_length=20, primary_key=True)
+    accession = models.CharField(max_length=15)
+    version = models.IntegerField()
+    ncbi_id = models.ForeignKey('Taxonomy', to_field='ncbi_id', db_column='ncbi_id')
+    length = models.IntegerField()
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'rfamseq'
+
+
+class FullRegion(models.Model):
+    id = models.AutoField(primary_key=True)
+    rfam_acc = models.ForeignKey('Family', to_field='rfam_acc', db_column='rfam_acc')
+    rfamseq_acc = models.ForeignKey('Rfamseq', to_field='rfamseq_acc', db_column='rfamseq_acc')
+    seq_start = models.IntegerField()
+    seq_end = models.IntegerField()
+    bit_score = models.FloatField()
+    evalue_score = models.FloatField()
+    cm_start = models.IntegerField()
+    cm_end = models.IntegerField()
+    truncated = models.CharField(max_length=1)
+    type = models.CharField(max_length=4, db_index=True)
+    is_significant = models.IntegerField(db_index=True)
+
+    class Meta:
+        db_table = 'full_region'
+
+
+class Taxonomy(models.Model):
+    ncbi_id = models.IntegerField(primary_key=True)
+    species = models.CharField(max_length=100)
+    tax_string = models.TextField()
+
+    class Meta:
+        db_table = 'taxonomy'
--- api/api/urls.py
+++ api/api/urls.py
@@ -16,35 +16,87 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
-
-
-
-
-
-from api.models import Genome
-from rest_framework import routers, serializers, viewsets
+from api.models import Genome, Family, Rfamseq, FullRegion, Taxonomy
+from rest_framework import routers, serializers, viewsets, generics
 
 # Serializers define the API representation.
 class GenomeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Genome
-        fields = ('upid', 'ncbi_id', 'length', 'description', 'scientific_name', 'common_name')
+        fields = '__all__'
 
 # ViewSets define the view behavior.
 class GenomeViewSet(viewsets.ModelViewSet):
     queryset = Genome.objects.all()
     serializer_class = GenomeSerializer
 
+class FamilySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Family
+        fields = '__all__'
+
+class FamilyViewSet(viewsets.ModelViewSet):
+    queryset = Family.objects.all()
+    serializer_class = FamilySerializer
+
+class RfamseqSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Rfamseq
+        fields = ('description', 'ncbi_id', 'rfamseq_acc', 'length')
+        depth = 1
+
+class RfamseqViewSet(viewsets.ModelViewSet):
+    queryset = Rfamseq.objects.all()
+    serializer_class = RfamseqSerializer
+
+class TaxonomySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Taxonomy
+        fields = '__all__'
+
+class TaxonomyViewSet(viewsets.ModelViewSet):
+    queryset = Taxonomy.objects.all()
+    serializer_class = TaxonomySerializer
+
+class FullRegionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = FullRegion
+        fields = ( 'bit_score', 'cm_end', 'cm_start', 'evalue_score', 'is_significant',
+        'rfam_acc', 'seq_end', 'seq_start', 'truncated')
+        depth = 1
+
+class FullRegionDetailView(generics.ListAPIView):
+    """
+    View details about Rfam hits.
+    Optionally filter using "start" and "end" URL query parameters.
+    """
+    serializer_class = FullRegionSerializer
+
+    def get_queryset(self):
+        rfamseq_acc = self.kwargs['rfamseq_acc']
+        queryset = FullRegion.objects.filter(rfamseq_acc=rfamseq_acc).all()
+        seq_start = self.request.query_params.get('start', None)
+        seq_end = self.request.query_params.get('end', None)
+        if seq_start and seq_end:
+            queryset = queryset.filter(seq_start=seq_start, seq_end=seq_end)
+        return queryset
+
+class FullRegionViewSet(viewsets.ModelViewSet):
+    queryset = FullRegion.objects.all()
+    serializer_class = FullRegionSerializer
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'genomes', GenomeViewSet)
+router.register(r'families', FamilyViewSet)
+router.register(r'rfamseq', RfamseqViewSet)
+router.register(r'taxonomy', TaxonomyViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^full_region/(?P<rfamseq_acc>.+)/$', FullRegionDetailView.as_view()),
 ]
